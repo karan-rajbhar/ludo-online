519652c81d21c565c196e89ddfb4c4b8b7388ede
Karan Rajbhar <kran@outlook.com>
2024-04-22 23:08:09 +0530

    removed unused code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-import LudoBoard from '../views/LudoBoard.vue'
+import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'LudoBoard',
-      component: LudoBoard
+      name: 'HomeView',
+      component: HomeView
     },
     
   ]
