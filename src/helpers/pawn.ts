708dd809b78392794efc8993dd5b0dfd181ad6cb
Karan Rajbhar <kran@outlook.com>
2024-04-22 23:19:24 +0530

    updated pawn and player class

--- src/helpers/pawn.ts
+++ src/helpers/pawn.ts
@@ -1,19 +1,39 @@
 class Pawn {
-    position: number;
-  
-    constructor(position: number) {
-      this.position = position;
+  position: number;
+  home: boolean;
+  captured: boolean;
+  start: boolean;
+
+  constructor(position: number) {
+    this.position = position;
+    this.home = false;
+    this.captured = false;
+    this.start = true;
+  }
+
+
+  move(diceNumber: number) {
+    if (this.start && diceNumber === 6) {
+      this.start = false;
+    }
+
+    if (!this.home && this.position + diceNumber >= 40) {
+      this.position = this.position + diceNumber - 40;
+      this.home = true;
+    } else {
+      this.position += diceNumber;
+    }
+    if (!this.start) {
+      // ... existing move logic ...
     }
   }
-  
-  class Player {
-    name: string;
-    color: string;
-    pawns: Pawn[];
-  
-    constructor(name: string, color: string) {
-      this.name = name;
-      this.color = color;
-      this.pawns = [new Pawn(0), new Pawn(0), new Pawn(0), new Pawn(0)];
-    }
+
+  reset() {
+    this.position = 0;
+    
   }
+  capture() {
+    this.position = 0;
+    this.captured = true;
+  }
+}
--- src/helpers/player.ts
+++ src/helpers/player.ts
@@ -1,42 +1,4 @@
-class Pawn {
-  position: number;
-  home: boolean;
-  captured: boolean;
-  start: boolean;
 
-  constructor(position: number) {
-    this.position = position;
-    this.home = false;
-    this.captured = false;
-    this.start = true;
-  }
-
-
-  move(diceNumber: number) {
-    if (this.start && diceNumber === 6) {
-      this.start = false;
-    }
-
-    if (!this.home && this.position + diceNumber >= 40) {
-      this.position = this.position + diceNumber - 40;
-      this.home = true;
-    } else {
-      this.position += diceNumber;
-    }
-    if (!this.start) {
-      // ... existing move logic ...
-    }
-  }
-
-  reset() {
-    this.position = 0;
-    
-  }
-  capture() {
-    this.position = 0;
-    this.captured = true;
-  }
-}
 
 export default class Player{
   name: string;
