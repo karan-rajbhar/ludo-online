6a78440494ca347f03c2d458a6a85166477af801
Karan Rajbhar <kran@outlook.com>
2024-05-01 20:22:02 +0530

    removed element-ui added naive-ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,60 @@
 <template>
 
   <div class="home">
-    <el-button type="primary" @click="dialogVisible = true"> Start Game</el-button>
+    <n-button type="primary" @click="dialogVisible = true"> Start Game
+    </n-button>
   </div>
 
-  <el-dialog v-model="dialogVisible" title="Select Number of Players" width="700" height="500"
-    :before-close="handleClose">
-    <el-tabs v-model="playerCount" class="demo-tabs" @tab-click="handleClick">
-      <el-tab-pane label="2P" name="2P">
-
-        <el-radio-group v-model="radio">
-          <el-radio :value="1">
-            <PawnPiece color="blue"> <el-input v-model="player_1_name" style="width: 240px" placeholder="Player 1" />
+  <n-modal v-model:show="dialogVisible">
+    <n-tabs v-model="playerCount" class="demo-tabs" @tab-click="handleClick">
+      <n-tab-pane label="2P" name="2P" class="select-tab
+      ">
+        <n-radio-group v-model="radio">
+          <n-radio :value="1">
+            <PawnPiece color="blue"> <n-input v-model="player_1_name" style="width: 240px" placeholder="Player 1" />
             </PawnPiece>
-            <PawnPiece color="green"> <el-input v-model="player_2_name" style="width: 240px" placeholder="Player 2" />
+            <PawnPiece color="green"> <n-input v-model="player_2_name" style="width: 240px" placeholder="Player 2" />
             </PawnPiece>
-          </el-radio>
-          <el-radio :value="2">
-            <PawnPiece color="red"> <el-input v-model="player_1_name" style="width: 240px" placeholder="Player 1" />
+          </n-radio>
+          <n-radio :value="2">
+            <PawnPiece color="red"> <n-input v-model="player_1_name" style="width: 240px" placeholder="Player 1" />
             </PawnPiece>
-            <PawnPiece color="yellow"> <el-input v-model="player_2_name" style="width: 240px" placeholder="Player 2" />
+            <PawnPiece color="yellow"> <n-input v-model="player_2_name" style="width: 240px" placeholder="Player 2" />
             </PawnPiece>
-          </el-radio>
+          </n-radio>
+        </n-radio-group>
 
-        </el-radio-group>
+      </n-tab-pane>
+      <n-tab-pane label="3P" name="3P">3P</n-tab-pane>
+      <n-tab-pane label="4P" name="4P">4P</n-tab-pane>
+    </n-tabs>
+  </n-modal>
 
-      </el-tab-pane>
-      <el-tab-pane label="3P" name="3P">3P</el-tab-pane>
-      <el-tab-pane label="4P" name="4P">4P</el-tab-pane>
-    </el-tabs>
-
-    <template #footer>
-      <div class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">
-          Confirm
-        </el-button>
-      </div>
-    </template>
-  </el-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { ElMessageBox } from 'element-plus'
-import { ElDialog } from 'element-plus'
-import type { TabsPaneContext } from 'element-plus'
 import PawnPiece from '@/components/PawnPiece.vue'
 
 const dialogVisible = ref(false)
 
-const handleClose = () => {
-  ElMessageBox.confirm('Are you sure you want to close this dialog?')
-    .then(() => {
-      dialogVisible.value = false
-    })
-    .catch(() => { })
-}
+
 
 const playerCount = ref('2P')
 const radio = ref(1)
 const player_1_name = ref('')
 const player_2_name = ref('')
 
-const handleClick = (tab: TabsPaneContext, event: Event) => {
+
+function handleClick(tab, event) {
   console.log(tab, event)
 }
+
+
+
 </script>
 
 
 <style scoped>
-#home-view {
-  min-height: 100vh; /* Adjust as needed */
-}
+
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +12,21 @@ export default defineConfig({
     vue(),
     vueJsx(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers: [NaiveUiResolver()]
+    })
   ],
   resolve: {
     alias: {
